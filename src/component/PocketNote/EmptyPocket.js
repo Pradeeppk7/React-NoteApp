bbafd1a46c0697e2decfe43dfd612268729d965f
“[Pradeep <[pradeepkumar.pk002@gmail.com]>
2023-09-11 01:46:43 +0530

    Display rack empty component added

--- src/component/PocketNote/EmptyPocket.js
+++ src/component/PocketNote/EmptyPocket.js
@@ -1,5 +1,7 @@
 import React from 'react';
-
+import bgimage from '../../assets/Emptypocket.png';
+import lock from '../../assets/Vector.png';
+import empty from './Pocketnote.module.css';
 // #region constants
 
 // #endregion
@@ -15,13 +17,24 @@ import React from 'react';
 // #region component
 
 /**
- * 
+ *
  */
 const EmptyPocket = () => {
-    return <div className='emptypocket'>
-        <img src="" alt="" />
-    </div>;
-}
+  return (
+      <div className={empty.emptypocket}>
+      <img className={empty.emptybg} src={bgimage} alt="" />
+      <p className={empty.head}>Pocket Notes</p>
+      <p className={empty.sub}>
+        Send and receive messages without keeping your phone online.<br></br>
+        Use Pocket Notes on up to 4 linked devices and 1 mobile phone
+      </p>
+      <div className={empty.bottomcontent}>
+              <img className={empty.emptylock} src={lock} alt="" />
+        <p className={empty.sub}>end-to-end encrypted</p>
+      </div>
+    </div>
+  );
+};
 
 // #endregion
 
