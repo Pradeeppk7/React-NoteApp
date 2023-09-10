84f93187a0ded20d165d94f7421d4a417514de30
“[Pradeep <[pradeepkumar.pk002@gmail.com]>
2023-09-10 20:09:11 +0530

    Textarea added with functionality

--- src/component/PocketNote/Pocketnote.js
+++ src/component/PocketNote/Pocketnote.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import note from './Pocketnote.module.css';
+import useNoteContext from '../../context/useNoteContext';
+import enterlogo from '../../assets/arrow.svg';
 // #region constants
 
 // #endregion
@@ -21,17 +23,96 @@ const defaultProps = {};
  *
  */
 const Pocketnote = () => {
+  const [text, setText] = useState('');
+  const [bgColor, setBgColor] = useState('#fff');
+  const [Title, setTitle] = useState('');
+  const { notes, setNotes, selectedNote } = useNoteContext();
+
+  const capitalizeFirst = (str) => {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  };
+
+  const getFirstLetters = (str) => {
+    const firstLetters = str
+      .split(' ')
+      .map((word) => word.charAt(0))
+      .join('')
+      .slice(0, 2)
+      .toUpperCase();
+
+    return firstLetters;
+    };
+    const handleText = (e) => {
+        setText(e.target.value);
+    }
+    const handleKey = (e) => {
+        if (e.key === "Enter") {
+          e.preventDefault();
+          handleSaveNotes();
+          }
+    }
+  useEffect(() => {
+    setNotes(JSON.parse(localStorage.getItem(selectedNote)) || []);
+    const groupNames = JSON.parse(localStorage.getItem('groupNames'));
+    const selectedGroupname = groupNames.find(
+      (item) => item.name === selectedNote
+    );
+    if (selectedNote) {
+      setBgColor(selectedGroupname.bgcolor);
+      setTitle(selectedGroupname.name);
+    }
+  }, [selectedNote, setNotes]);
+
+  const getDate = () => {
+    return new Date().toLocaleDateString("en-GB", {
+      day: "numeric",
+      month: "numeric",
+      year: "numeric",
+    })
+  }
+  const handleSaveNotes = () => {
+    if (!text.trim()) {
+      return;
+    }
+    const notes = JSON.parse(localStorage.getItem(selectedNote)) || [];
+    const newNoteObj = {
+      id: Date.now(),
+      title: selectedNote,
+      content: text.trim(),
+      date:getDate(),
+      time: new Date().toLocaleTimeString(),
+    };
+    notes.push(newNoteObj);
+    localStorage.setItem(selectedNote, JSON.stringify(notes));
+    setText("");
+    setNotes(notes);
+  };
   return (
     <div className={note.section}>
       <div className={note.layer}>
         <div className={note.heading}>
-          <div className={note.displayname}>
-            <h2>WD</h2>
+          <div
+            className={note.displayname}
+            style={{ backgroundColor: bgColor }}
+          >
+            <h2>{getFirstLetters(Title)}</h2>
           </div>
-            <div className={note.title}><h2>Cuvette note</h2></div>
+          <div className={note.title}>
+            <h2>{capitalizeFirst(Title)}</h2>
+          </div>
         </div>
-        <div className={note.noterack}>notes component</div>
-        <div className={note.textarea}></div>
+        <div className={note.noterack}>
+          
+        </div>
+              <div className={note.textarea}>
+              <textarea
+          value={text}
+          placeholder="Enter your text here......"
+          onChange={handleText}
+          onKeyDown={handleKey}
+        ></textarea>
+        <img src = {enterlogo} onClick={handleSaveNotes} alt="My Happy SVG"/>
+        </div>
       </div>
     </div>
   );
